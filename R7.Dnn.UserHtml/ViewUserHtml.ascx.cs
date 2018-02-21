748bdc72d40389bfc2c5ef79069a410b0ee3cbbc
roman-yagodin <roman.yagodin@gmail.com>
2018-02-21 09:45:15 +0300

    Change license to MIT, update copyright
    Copyright will be replaced on registration of v1.0 in separate branch.

--- R7.Dnn.UserHtml/ViewUserHtml.ascx.cs
+++ R7.Dnn.UserHtml/ViewUserHtml.ascx.cs
@@ -1,23 +1,28 @@
 //
-//  ViewUserHtml.ascx.cs
+// ViewUserHtml.ascx.cs
 //
-//  Author:
+// Author:
 //       Roman M. Yagodin <roman.yagodin@gmail.com>
 //
-//  Copyright (c) 2018 Volgograd State Agricultural University
+// Copyright (c) 2018 Roman M. Yagodin
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 using System;
 using System.Collections.Generic;
@@ -34,6 +39,7 @@ using R7.Dnn.Extensions.Modules;
 using R7.Dnn.UserHtml.Data;
 using R7.Dnn.UserHtml.Models;
 using R7.Dnn.UserHtml.ViewModels;
+using R7.Dnn.Extensions.Utilities;
 
 namespace R7.Dnn.UserHtml
 {
@@ -70,16 +76,11 @@ namespace R7.Dnn.UserHtml
                     // TODO: Add support for userid querystring parameter?
                     // TODO: Don't show content on profile page of different user.
                     if (Request.IsAuthenticated) {
-                        var dataProvider = new UserHtmlDataProvider ();
-                        var userHtml = dataProvider.GetUserHtml (UserId, ModuleId);
-                        if (userHtml != null && !string.IsNullOrEmpty (userHtml.UserHtml)) {
-                            litUserHtml.Text = HttpUtility.HtmlDecode (userHtml.UserHtml);
-                        }
-                        else if (IsEditable) {
-                            pnlUser.Visible = true;
+                        if (!IsOnProfilePage () || IsOnOwnProfilePage ()) {
+                            ShowContent ();
+                            pnlUser.Visible = IsEditable;
                         }
                         else {
-                            // TODO: Display default content?
                             HideModule ();
                         }
                     }
@@ -90,6 +91,25 @@ namespace R7.Dnn.UserHtml
             }
             catch (Exception ex) {
                 Exceptions.ProcessModuleLoadException (this, ex);
+            }
+        }
+
+        int? GetUserProfileId () => TypeUtils.ParseToNullable<int> (Request.QueryString ["userid"]);
+
+        // TODO: Use profile page from portal settings?
+        bool IsOnProfilePage () => GetUserProfileId () != null;
+
+        bool IsOnOwnProfilePage () => IsOnProfilePage () && GetUserProfileId ().Value == UserId;
+
+        void ShowContent ()
+        {
+            var dataProvider = new UserHtmlDataProvider ();
+            var userHtml = dataProvider.GetUserHtml (UserId, ModuleId);
+            if (userHtml != null && !string.IsNullOrEmpty (userHtml.UserHtml)) {
+                litUserHtml.Text = HttpUtility.HtmlDecode (userHtml.UserHtml);
+            }
+            else {
+                // TODO: Display default content?
             }
         }
 
